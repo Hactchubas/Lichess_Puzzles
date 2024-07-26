5a9723a596ff7d52923733613291799acebd014d
Kasoma <kaua.maia@sienacompany.com>
2024-07-26 13:24:14 -0300

    First Commit

---
+++ src/main/kotlin/com/lichess/puzzles/PuzzlesApplication.kt
@@ -0,0 +1,12 @@
+package com.lichess.puzzles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PuzzlesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PuzzlesApplication>(*args)
+	//
+}
---
+++ src/test/kotlin/com/lichess/puzzles/PuzzlesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lichess.puzzles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PuzzlesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
